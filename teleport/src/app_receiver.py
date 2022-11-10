daf64fe94bc39e975e3e2acaaa0e45ea1064b7c0
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-11-10 14:35:41 +0100

    Write first application
    That creates an entangled pair between Sender and Receiver, measures
    it on each side, and prints the measurement result on each side

--- teleport/src/app_receiver.py
+++ teleport/src/app_receiver.py
@@ -1,7 +1,20 @@
+from netqasm.sdk.external import NetQASMConnection
+from netqasm.sdk import EPRSocket
+
+
 def main(app_config=None):
-    # Put your code here
-    return {}
+    # Specify an EPR socket to Sender
+    epr_socket = EPRSocket("Sender")
 
-
-if __name__ == "__main__": 
-    main()
+    receiver = NetQASMConnection(
+        "Receiver",
+        log_config=app_config.log_config,
+        epr_sockets=[epr_socket],
+    )
+    with receiver:
+        # Receive an entangled pair using the EPR socket to Sender
+        q_ent = epr_socket.recv()[0]
+        # Measure the qubit
+        m = q_ent.measure()
+    # Print the outcome
+    print(f"Receiver's outcome is: {m}")
--- teleport/src/app_sender.py
+++ teleport/src/app_sender.py
@@ -1,7 +1,20 @@
+from netqasm.sdk.external import NetQASMConnection
+from netqasm.sdk import EPRSocket
+
+
 def main(app_config=None):
-    # Put your code here
-    return {}
+    # Specify an EPR socket to Receiver
+    epr_socket = EPRSocket("Receiver")
 
-
-if __name__ == "__main__": 
-    main()
+    sender = NetQASMConnection(
+        "Sender",
+        log_config=app_config.log_config,
+        epr_sockets=[epr_socket],
+    )
+    with sender:
+        # Create an entangled pair using the EPR socket to Receiver
+        q_ent = epr_socket.create()[0]
+        # Measure the qubit
+        m = q_ent.measure()
+    # Print the outcome
+    print(f"Sender's outcome is: {m}")

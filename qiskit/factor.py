f679207cbf2afe777291915e125a5a6726a580c6
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-11-21 10:24:06 +0100

    Put in class

--- qiskit/factor.py
+++ qiskit/factor.py
@@ -7,12 +7,9 @@ import sys
 
 def main():
     args = parse_command_line_arguments()
-    try:
-        divisor = find_divisor(args)
-    except Exception as exception:
-        print(exception, file=sys.stderr)
-        sys.exit(1)
-    print(f'The divisor is {divisor}')
+    divisor_finder = DivisorFinder(args.number)
+    divisor = divisor_finder.find_divisor()
+    print(f'Divisor is {divisor}')
 
 
 def parse_command_line_arguments():
@@ -21,44 +18,45 @@ def parse_command_line_arguments():
         'number',
         help='The number for which to find a divisor')
     args = parser.parse_args()
+    try:
+        args.number = int(args.number)
+    except ValueError:
+        invalid_number_argument(args.number, 'Must be an integer')
+    if args.number < 3:
+        invalid_number_argument(args.number, 'Must be 3 or greater')
     return args
 
 
-def find_divisor(args):
-    number = get_number(args)
-    divisor = None
-    while divisor is None:
-        a = random.randint(2, number-1)
-        divisor = try_find_divisor_for_a(number, a)
-    return number
-
-
-def get_number(args):
-    number_str = args.number
-    try:
-        number = int(number_str)
-    except ValueError:
-        invalid_number_argument(number_str, 'Must be an integer')
-    if number < 3:
-        invalid_number_argument(number_str, 'Must be 3 or greater')
-    return number
-
-
 def invalid_number_argument(number_str, reason):
-    raise AttributeError(f'Invalid number argument {number_str}: {reason}')
+    print(f'Invalid number argument {number_str}: {reason}', file=sys.stderr)
+    sys.exit(1)
 
 
-def try_find_divisor_for_a(number, a):
-    print(f'try_find_divisor_for_a {number=} {a=}')
-    k = greatest_common_divisor(a, number)
-    print(f'{k=}')
-    return None
+class DivisorFinder:
 
+    def __init__(self, number):
+        self.number = number
 
-def greatest_common_divisor(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+    def find_divisor(self):
+        divisor = None
+        while divisor is None:
+            a = random.randint(2, self.number - 1)
+            divisor = self.try_find_divisor_for_a(a)
+        return divisor
+
+    def try_find_divisor_for_a(self, a):
+        print(f'try_find_divisor_for_a {a=}')
+        k = self.greatest_common_divisor(a, self.number)
+        print(f'{k=}')
+        if k != 1:
+            return k
+        return None
+
+    @staticmethod
+    def greatest_common_divisor(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
 
 
 if __name__ == '__main__':

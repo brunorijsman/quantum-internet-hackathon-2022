c9aa94c549786bd293da6115f81006f5d66f8a40
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-12-02 12:43:11 +0100

    2nd page presentation

--- qiskit/cluster.py
+++ qiskit/cluster.py
@@ -161,8 +161,6 @@ class Processor:
 
 class Cluster:
 
-    # TODO Add a swap parameter to enable or disable the final swaps
-
     def __init__(self, nr_processors, total_nr_qubits, flag):
         assert total_nr_qubits % nr_processors == 0, \
             'Total nr qubits {total_nr_qubits} must be multiple of nr processors {nr_processors}'

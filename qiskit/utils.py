7b1387d0a1c430ce37a195e504b85adddb44c4e3
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-12-13 23:05:21 +0100

    Fix lint errors

--- qiskit/utils.py
+++ qiskit/utils.py
@@ -1,8 +1,25 @@
-from qiskit.quantum_info import DensityMatrix
+"""
+General utilities for QFTs in Qiskit.
+"""
+
 from datetime import datetime
+from qiskit.quantum_info import DensityMatrix
 
 
 def reverse_bit_order(nr_bits, value):
+    """
+    Reverse the bits in a value.
+
+    Parameters
+    ----------
+    nr_bits: The number of bits for the value (we need to specify this because the value might have
+        leading zeroes).
+    value: The value to be reversed.
+
+    Returns
+    -------
+    The value of the reversed bit order.
+    """
     reversed_value = 0
     for _ in range(nr_bits):
         bit = value & 1
@@ -13,6 +30,19 @@ def reverse_bit_order(nr_bits, value):
 
 
 def density_matrix_reverse_bit_order(density_matrix):
+    """
+    Convert a density matrix into the corresponding density bit matrix where the bit order of the
+    states is reversed. This is needed to convert density matrices as computed by Qiskit into
+    density matrices as computed by Quantum Network Explorer, and vice versa.
+
+    Parameters
+    ----------
+    density_matrix: A density matrix.
+
+    Returns
+    -------
+    The corresponding density matrix with the bit order of the states reversed.
+    """
     nr_qubits = len(density_matrix.dims())
     dm_size = nr_qubits**2
     reversed_dm = []
@@ -28,26 +58,48 @@ def density_matrix_reverse_bit_order(density_matrix):
 
 
 def density_matrix_pretty_print(density_matrix):
+    """
+    Pretty print a density matrix.
+
+    Parameters
+    ----------
+    density_matrix: The density matrix to be printed.
+    """
     nr_qubits = len(density_matrix.dims())
     for dimension in density_matrix.dims():
         assert dimension == 2, "dimension of each subsystem must be 2"
     dm_size = 2**nr_qubits
-    for r in range(dm_size):
-        for c in range(dm_size):
-            value = density_matrix.data[r][c]
-            rv = value.real
-            iv = value.imag
-            print(f"{rv:>6.3f} {iv:>6.3f}j    ", end="")
+    for row in range(dm_size):
+        for column in range(dm_size):
+            value = density_matrix.data[row][column]
+            real_value = value.real
+            imaginary_value = value.imag
+            print(f"{real_value:>6.3f} {imaginary_value:>6.3f}j    ", end="")
         print()
 
 
-def density_matrix_print_to_file(file_name, producer_name, nr_qubits, dm, input, swaps):
-    with open(file_name, "w") as f:
-        print(producer_name, file=f)
-        print(f"{datetime.now()}", file=f)
-        print(f"{nr_qubits}", file=f)
-        print(f"{input}", file=f)
-        print(f"{swaps}", file=f)
-        for r in range(nr_qubits):
-            for c in range(nr_qubits):
-                print(dm[r][c], file=f)
+def density_matrix_print_to_file(
+    file_name, producer_name, nr_qubits, density_matrix, input_value, final_swaps
+):
+    """
+    Print a density matrix to a file. This is used to compare one density matrix with another
+    density matrix.
+
+    Parameters
+    ----------
+    file_name: The name of the file that the density matrix should be printed to.
+    producer_name: The name of the algorithm that produced the density matrix.
+    nr_qubits: The number of qubits that the density matrix describes.
+    density_matrix: The density matrix to be printed.
+    input_value: The input value to the algorithm that produced the density matrix.
+    final_swap: Whether the QFT algorithm that produced the density matrix did final swaps.
+    """
+    with open(file_name, "w", encoding="utf-8") as file:
+        print(producer_name, file=file)
+        print(f"{datetime.now()}", file=file)
+        print(f"{nr_qubits}", file=file)
+        print(f"{input_value}", file=file)
+        print(f"{final_swaps}", file=file)
+        for row in range(nr_qubits):
+            for column in range(nr_qubits):
+                print(density_matrix[row][column], file=file)

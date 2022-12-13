1a76f37508cd48266aa03d508b402ea92257089a
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-12-13 16:44:40 +0100

    Fix some lint errors

--- qiskit/qft.py
+++ qiskit/qft.py
@@ -1,37 +1,45 @@
+"""
+A non-distributed implementation of the Quantum Fourier Transformation (QFT).
+"""
+
 from numpy import pi
+from qiskit_textbook.tools import array_to_latex
 from qiskit import Aer, QuantumCircuit, transpile
 from qiskit.quantum_info import DensityMatrix
 from qiskit.visualization import plot_bloch_multivector, plot_state_city
-from qiskit_textbook.tools import array_to_latex
 
 
 class QFT:
-    def __init__(self, n, swaps=True):
-        self.n = n
+    """
+    A non-distributed implementation of the Quantum Fourier Transformation (QFT).
+    """
+
+    def __init__(self, nr_qubits, swaps=True):
+        self.nr_qubits = nr_qubits
         self.swaps = swaps
-        self.qc = QuantumCircuit(n)
+        self.qc = QuantumCircuit(nr_qubits)
         self.qc_with_input = None
         self.simulator = None
-        self.results = None
+        self.result = None
         self._make_qft()
 
     def _make_qft(self):
-        self._add_qft_rotations(self.n)
+        self._add_qft_rotations(self.nr_qubits)
         if self.swaps:
             self._add_qft_swaps()
 
-    def _add_qft_rotations(self, n):
-        if n == 0:
+    def _add_qft_rotations(self, remaining_nr_qubits):
+        if remaining_nr_qubits == 0:
             return
-        n -= 1
-        self.qc.h(n)
-        for qubit in range(n):
-            self.qc.cp(pi / 2 ** (n - qubit), qubit, n)
-        self._add_qft_rotations(n)
+        remaining_nr_qubits -= 1
+        self.qc.h(remaining_nr_qubits)
+        for qubit in range(remaining_nr_qubits):
+            self.qc.cp(pi / 2 ** (remaining_nr_qubits - qubit), qubit, remaining_nr_qubits)
+        self._add_qft_rotations(remaining_nr_qubits)
 
     def _add_qft_swaps(self):
-        for qubit in range(self.n // 2):
-            self.qc.swap(qubit, self.n - qubit - 1)
+        for qubit in range(self.nr_qubits // 2):
+            self.qc.swap(qubit, self.nr_qubits - qubit - 1)
 
     def circuit_diagram(self, with_input=False):
         if with_input:
@@ -65,9 +73,9 @@ class QFT:
             return None
         return plot_state_city(self.result.get_statevector())
 
-    def run(self, input, shots=10000):
-        self.qc_with_input = QuantumCircuit(self.n)
-        bin_value = bin(input)[2:].zfill(self.n)
+    def run(self, input_value, shots=10000):
+        self.qc_with_input = QuantumCircuit(self.nr_qubits)
+        bin_value = bin(input_value)[2:].zfill(self.nr_qubits)
         self.qc_with_input.initialize(bin_value, self.qc_with_input.qubits)
         self.qc_with_input = self.qc_with_input.compose(self.qc)
         self.simulator = Aer.get_backend("aer_simulator")

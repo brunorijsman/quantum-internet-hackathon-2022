09816aac53e9a20478c92f1e0c889ba06e418255
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-12-13 13:25:21 +0100

    Add lint script

---
+++ scripts/lint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# set -e
+
+TRUE=1
+FALSE=0
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+REPO_ROOT_DIR=${SCRIPT_DIR}/..
+
+LINTED_DIRS="qiskit"
+ALL_LINTS_OK=$TRUE
+
+for DIR in $LINTED_DIRS; do
+
+    echo "Lint $DIR using black"
+    black --check $REPO_ROOT_DIR/$DIR
+    if [ "$?" -ne 0 ]; then
+        echo $OUTPUT
+        ALL_LINTS_OK=$FALSE
+    fi
+
+done
+
+if [[ $ALL_LINTS_OK == $FALSE ]]; then
+    echo "At least one lint failed"
+    exit 1
+fi
+echo "All lint checks passed"
+
+exit 0

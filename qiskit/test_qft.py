44fe8ca78c38d127e7c5acd5cf9fbfcff6adbcd7
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2022-12-19 10:15:33 +0100

    Add more QFT test cases

--- qiskit/test_qft.py
+++ qiskit/test_qft.py
@@ -10,6 +10,8 @@ from qiskit.quantum_info import Statevector
 ONE_OVER_SQRT2 = 1.0 / sqrt(2.0)
 PLUS_STATE = Statevector([ONE_OVER_SQRT2, ONE_OVER_SQRT2])
 MINUS_STATE = Statevector([ONE_OVER_SQRT2, -1.0 * ONE_OVER_SQRT2])
+PLUS_PLUS_STATE = PLUS_STATE.tensor(PLUS_STATE)
+PLUS_PLUS_PLUS_PLUS_STATE = PLUS_PLUS_STATE.tensor(PLUS_PLUS_STATE)
 
 
 def test_monolithic_qft_one_qubit():
@@ -24,3 +26,23 @@ def test_monolithic_qft_one_qubit():
     qft.run(input_number=1)
     statevector = qft.main_statevector()
     assert state_vectors_are_same(statevector, MINUS_STATE)
+
+
+def test_monolithic_qft_two_qubits():
+    """
+    Test monolothic (non-distributed) QFT, two qubits.
+    """
+    qft = QFT(total_nr_qubits=2)
+    qft.run(input_number=0)
+    statevector = qft.main_statevector()
+    assert state_vectors_are_same(statevector, PLUS_PLUS_STATE)
+
+
+def test_monolithic_qft_four_qubits():
+    """
+    Test monolothic (non-distributed) QFT, four qubits.
+    """
+    qft = QFT(total_nr_qubits=4)
+    qft.run(input_number=0)
+    statevector = qft.main_statevector()
+    assert state_vectors_are_same(statevector, PLUS_PLUS_PLUS_PLUS_STATE)

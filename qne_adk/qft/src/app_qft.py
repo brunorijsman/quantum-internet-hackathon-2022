9c510c72704ea40d57a284c41874b77bdf4be232
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2023-01-16 17:10:50 -0600

    More lint checks

--- qne_adk/qft/src/app_qft.py
+++ qne_adk/qft/src/app_qft.py
@@ -1,79 +1,167 @@
+"""
+Monolithic (non-distributed) implementation of the quantum Fourier transformation in QNE-ADK.
+"""
+
+from datetime import datetime
 from netqasm.logging.output import get_new_app_logger
 from netqasm.sdk.external import NetQASMConnection, get_qubit_state
 from netqasm.sdk import Qubit
 
-from datetime import datetime
 
+def apply_qft(app_logger, connection, qubits, input_size, input_value, do_final_swaps):
+    """
+    Apply a quantum Fourier transformation.
 
-def apply_qft(app_logger, conn, qubits, n, value):
+    Parameters
+    ----------
+    app_logger: The application logger.
+    connection: The NetQASM connection.
+    qubits: A map of Qubit objects, indexed by qubit index number. After this function returns,
+        these qubits contain the quantum state that is the result of the QFT.
+    input_size: The number of qubits in the input value for the QFT.
+    input_value: Assume that all input values for qubits are |0> or |1>, treat these qubits
+        as the binary encoding of the input value as a number.
+    do_final_swaps: If True, do the final swaps for the QFT. If False, don't.
+    """
     app_logger.log("apply qft")
-    apply_qft_value(app_logger, conn, qubits, n, value)
-    apply_qft_rotations(app_logger, conn, qubits, n)
-    apply_qft_swaps(app_logger, conn, qubits, n)
+    assert len(qubits) == input_size
+    apply_qft_value(app_logger, qubits, input_size, input_value)
+    apply_qft_rotations(app_logger, connection, qubits, input_size)
+    if do_final_swaps:
+        apply_qft_swaps(app_logger, qubits, input_size)
 
 
-def apply_qft_value(app_logger, conn, qubits, n, value):
-    app_logger.log(f"apply qft value {n=} {value=}")
-    for i in range(n):
-        bit = value & 1
-        value >>= 1
-        app_logger.log(f"bit {i} = {bit}")
+def apply_qft_value(app_logger, qubits, input_size, input_value):
+    """
+    Apply a circuit that sets the qubits to the |0> and |1> values determined by the input_value.
+
+    Parameters
+    ----------
+    app_logger: The application logger.
+    qubits: A map of Qubit objects, indexed by qubit index number. After this function returns,
+        these qubits are set to |0> or |1>, according to the input_value.
+    input_size: The number of qubits in the input value for the QFT.
+    input_value: Assume that all input values for qubits are |0> or |1>, treat these qubits
+        as the binary encoding of the input value as a number.
+    """
+    app_logger.log(f"apply qft value {input_size=} {input_value=}")
+    assert len(qubits) == input_size
+    for bit_index in range(input_size):
+        bit = input_value & 1
+        input_value >>= 1
+        app_logger.log(f"bit {bit_index} = {bit}")
         if bit:
-            app_logger.log(f"X gate qubit {i}")
-            qubits[i].X()
+            app_logger.log(f"X gate qubit {bit_index}")
+            qubits[bit_index].X()
 
 
-def apply_qft_rotations(app_logger, conn, qubits, n):
-    if n == 0:
+def apply_qft_rotations(app_logger, connection, qubits, remaining_nr_qubits):
+    """
+    Apply the controlled Z-rotations gates as part of the quantum Fourier transformation.
+
+    Parameters
+    ----------
+    app_logger: The application logger.
+    connection: The NetQASM connection.
+    qubits: A map of Qubit objects, indexed by qubit index number. After this function returns,
+        these qubits contain the quantum state that is the result of the QFT.
+    remaining_nr_qubits: The remaining number of qubits for which to do the controlled Z-rotation.
+    """
+    if remaining_nr_qubits == 0:
         return
-    app_logger.log(f"apply qft rotations {n=}")
-    n -= 1
-    app_logger.log(f"hadamard qubit {n}")
-    qubits[n].H()
-    for i in range(n):
+    app_logger.log(f"apply qft rotations {remaining_nr_qubits=}")
+    remaining_nr_qubits -= 1
+    app_logger.log(f"hadamard qubit {remaining_nr_qubits}")
+    qubits[remaining_nr_qubits].H()
+    for qubit_index in range(remaining_nr_qubits):
         app_logger.log(
-            f"controlled phase control qubit {i} and target qubit {n} "
-            f"by angle pi/{2 ** (n - i)}"
+            f"controlled phase control qubit {qubit_index} and target qubit {remaining_nr_qubits} "
+            f"by angle pi/{2 ** (remaining_nr_qubits - qubit_index)}"
         )
-        qubits[i].crot_Z(qubits[n], n=1, d=n - i)
-    apply_qft_rotations(app_logger, conn, qubits, n)
+        qubits[qubit_index].crot_Z(
+            qubits[remaining_nr_qubits], n=1, d=remaining_nr_qubits - qubit_index
+        )
+    apply_qft_rotations(app_logger, connection, qubits, remaining_nr_qubits)
 
 
-def apply_qft_swaps(app_logger, conn, qubits, n):
+def apply_qft_swaps(app_logger, qubits, nr_qubits):
+    """
+    Apply the controlled Z-rotations gates as part of the quantum Fourier transformation.
+
+    Parameters
+    ----------
+    app_logger: The application logger.
+    connection: The NetQASM connection.
+    qubits: A map of Qubit objects, indexed by qubit index number. After this function returns,
+        these qubits contain the quantum state that is the result of the QFT.
+    nr_qubits: The number of qubits.
+    """
     app_logger.log("apply qft swaps")
-    for i1 in range(n // 2):
-        i2 = n - i1 - 1
-        app_logger.log(f"swap qubit {i1} with qubit {i2} (XXX not implemented)")
-        # TODO
+    assert len(qubits) == nr_qubits
+    for qubit_index_1 in range(nr_qubits // 2):
+        qubit_index_2 = nr_qubits - qubit_index_1 - 1
+        app_logger.log(f"swap qubit {qubit_index_1} with qubit {qubit_index_2}")
+        # NetQASM does not yet natively support a SWAP gate; instead construct a SWAP gate out of
+        # three CNOT gates as described here: https://algassert.com/post/1717
+        # See issue #47 for details: https://github.com/QuTech-Delft/netqasm/issues/47
+        qubits[qubit_index_1].cnot(qubit_index_2)
+        qubits[qubit_index_2].cnot(qubit_index_1)
+        qubits[qubit_index_1].cnot(qubit_index_2)
+
+
+def write_density_matrix_to_file(app_logger, qubits, input_size, input_value, do_final_swaps):
+    """
+    Write the density matrix for the qubits to a file, including some metadata.
+
+    Parameters
+    ----------
+    app_logger: The application logger.
+    qubits: A map of Qubit objects, indexed by qubit index number, for which to write the density
+        matrix.
+    input_size: The number of qubits in the input value for the QFT.
+    input_value: Assume that all input values for qubits are |0> or |1>, treat these qubits
+        as the binary encoding of the input value as a number.
+    do_final_swaps: If True, do the final swaps for the QFT. If False, don't.
+    """
+    density_matrix = get_qubit_state(qubits[0], reduced_dm=False)
+    app_logger.log(f"density matrix is {density_matrix}")
+    app_logger.log("writing density matrix to qne_dm.txt")
+    # TODO: Determine dir in a better way
+    dir_name = "/Users/brunorijsman/git-personal/quantum-internet-hackathon-2022"
+    with open(f"{dir_name}/qne_dm.txt", "w", encoding="utf-8") as file:
+        print("QNE-ADM density matrix", file=file)
+        print(f"{datetime.now()}", file=file)
+        print(f"{input_size}", file=file)
+        print(f"{input_value}", file=file)
+        print(f"{do_final_swaps}", file=file)
+        for row_index in range(input_size):
+            for column_index in range(input_size):
+                print(density_matrix[row_index][column_index], file=file)
 
 
 def main(app_config=None):
+    """
+    The application main function.
+    """
+    # TODO: Make input_size and input_value application parameters
     app_logger = get_new_app_logger(app_name=app_config.app_name, log_config=app_config.log_config)
     app_logger.log("qft starts")
-    n = 3
-    app_logger.log(f"{n=}")
-    value = 1
-    app_logger.log(f"{value=}")
-    conn = NetQASMConnection("qft", log_config=app_config.log_config, epr_sockets=[], max_qubits=n)
-    with conn:
-        app_logger.log(f"qft creates register of {n} qubits")
+    input_size = 3
+    app_logger.log(f"{input_size=}")
+    input_value = 1
+    app_logger.log(f"{input_value=}")
+    do_final_swaps = True
+    app_logger.log(f"{do_final_swaps=}")
+    connection = NetQASMConnection(
+        "qft", log_config=app_config.log_config, epr_sockets=[], max_qubits=input_size
+    )
+    with connection:
+        app_logger.log(f"qft creates register of {input_size} qubits")
         qubits = {}
-        for i in range(n):
-            qubits[i] = Qubit(conn)
-        apply_qft(app_logger, conn, qubits, n, value)
-        conn.flush()
-        density_matrix = get_qubit_state(qubits[0], reduced_dm=False)
-        app_logger.log(f"density matrix for qubit {i} = {density_matrix}")
-        app_logger.log("writing density matrix to qne_dm.txt")
-        dir = "/Users/brunorijsman/git-personal/quantum-internet-hackathon-2022"
-        with open(f"{dir}/qne_dm.txt", "w") as f:
-            print("QNE-ADM density matrix", file=f)
-            print(f"{datetime.now()}", file=f)
-            print(f"{n}", file=f)
-            print(f"{value}", file=f)
-            print("False", file=f)
-            for r in range(n):
-                for c in range(n):
-                    print(density_matrix[r][c], file=f)
+        for qubit_index in range(input_size):
+            qubits[qubit_index] = Qubit(connection)
+        apply_qft(app_logger, connection, qubits, input_size, input_value, do_final_swaps)
+        connection.flush()
+    write_density_matrix_to_file(app_logger, qubits, input_size, input_value, do_final_swaps)
     app_logger.log("qft ends")
-    return {"n": n, "value": value}
+    return {"n": input_size, "value": input_value}
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -12,6 +12,7 @@ LINTED_DIRS+=" qne_adk"
 LINTED_DIRS+=" qne_adk/entanglement/src"
 LINTED_DIRS+=" qne_adk/teleport/src"
 LINTED_DIRS+=" qne_adk/just_crotz/src"
+LINTED_DIRS+=" qne_adk/qft/src"
 ALL_LINTS_OK=$TRUE
 
 for DIR in $LINTED_DIRS; do

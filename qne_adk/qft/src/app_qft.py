b6da0771db2542f10b6dd2da300fe28511f83329
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2023-01-19 15:45:12 -0600

    Fix black error

--- qne_adk/qft/src/app_qft.py
+++ qne_adk/qft/src/app_qft.py
@@ -136,7 +136,9 @@ def main(input_size, input_value, app_config=None):
         density_matrix = get_qubit_state(qubits[0], reduced_dm=False)
         app_logger.log("qft output density matrix")
         write_density_matrix_to_log(app_logger, density_matrix)
-        file_name = write_density_matrix_to_file("qne", "monolithic", input_size, input_value, density_matrix)
+        file_name = write_density_matrix_to_file(
+            "qne", "monolithic", input_size, input_value, density_matrix
+        )
         app_logger.log(f"wrote density matrix to {file_name}")
     app_logger.log("qft ends")
     return {"input_size": input_size, "input_value": input_value}

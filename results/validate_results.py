4c6d909c312ceb21194c996bfd49a14b083fa369
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2023-01-19 17:07:17 -0600

    Finish consistency checks (they fail)

--- results/validate_results.py
+++ results/validate_results.py
@@ -42,18 +42,40 @@ def read_all_experiment_results(results_dir):
 def validate_all_experiment_results(all_experiment_results):
     """
     Validate all experiment results.
+
+    Parameters
+    ----------
+    all_experiment_results: The results of all experiments.
+
+    Returns
+    -------
+    True if all experiment results are consistent with each other, False if not.
     """
+    all_consistent = True
     for experiment_results in all_experiment_results:
-        validate_one_experiment_results(experiment_results, all_experiment_results)
+        consistent = validate_one_experiment_results(experiment_results, all_experiment_results)
+        all_consistent = all_consistent and consistent
+    return all_consistent
 
 
 def validate_one_experiment_results(experiment_results, all_experiment_results):
     """
     Validate one experiment results against the other experiment results for consistency.
+
+    Parameters
+    ----------
+    experiment_results: The results of the experiment that is to be compared against all other
+        experiment results.
+    all_experiment_results: The results of all experiments to compare against.
+
+    Returns
+    -------
+    True if all experiment results are consistent with each other, False if not.
     """
     file_name = experiment_results["file_name"]
     data = experiment_results["data"]
     print(f"Validate {file_name}")
+    all_consistent = True
     for other_experiment_results in all_experiment_results:
         other_file_name = other_experiment_results["file_name"]
         other_data = other_experiment_results["data"]
@@ -65,8 +87,41 @@ def validate_one_experiment_results(experiment_results, all_experiment_results):
             continue
         if data["input_value"] != other_data["input_value"]:
             continue
-        print(f"  Compare with {other_file_name}")
+        consistent = check_consistency(experiment_results, other_experiment_results)
+        if consistent:
+            print(f"  Compare with {other_file_name}: consistent")
+        else:
+            print(f"  Compare with {other_file_name}: NOT consistent")
+        all_consistent = all_consistent and consistent
+    return all_consistent
 
+
+def check_consistency(experiment_results_1, experiment_results_2):
+    """
+    Check whether two experiment results are consistent with each other, i.e. whether they
+    produced the same output density matrix.
+
+    Parameters
+    ----------
+    experiment_results_1: The first experiment results to be compared.
+    experiment_results_2: The second experiment results to be compared.
+
+    Returns
+    -------
+    True if the results are consistent with each other, False if not
+    """
+    max_delta = 0.001
+    density_matrix_1 = experiment_results_1["data"]["density_matrix"]
+    density_matrix_2 = experiment_results_2["data"]["density_matrix"]
+    assert len(density_matrix_1) == len(density_matrix_2)
+    for row_1, row_2 in zip(density_matrix_1, density_matrix_2):
+        assert len(row_1) == len(row_2)
+        for value_1, value_2 in zip(row_1, row_2):
+            if abs(value_1["real"] - value_2["real"]) > max_delta:
+                return False
+            if abs(value_1["imag"] - value_2["imag"]) > max_delta:
+                return False
+    return True
 
 
 def main():
@@ -75,7 +130,11 @@ def main():
     """
     args = parse_command_line_arguments()
     all_experiment_results = read_all_experiment_results(args.results_dir)
-    validate_all_experiment_results(all_experiment_results)
+    all_consistent = validate_all_experiment_results(all_experiment_results)
+    if all_consistent:
+        print("All experimental results are consistent with each other")
+    else:
+        print("There is at least one inconsistency in the experimental results")
 
 
 if __name__ == "__main__":

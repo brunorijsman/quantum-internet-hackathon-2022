bd756460da663a51dbde39f37e4a38ab25a93b5c
Bruno Rijsman (Personal) <brunorijsman@gmail.com>
2023-01-23 15:35:12 -0600

    Make functions local

--- qne_adk/qft2/src/app_processor0.py
+++ qne_adk/qft2/src/app_processor0.py
@@ -4,7 +4,8 @@ This is the program for the first of two processors.
 """
 
 import processor
-import numpy
+
+# import numpy
 
 
 def main(app_config=None):
@@ -16,31 +17,14 @@ def main(app_config=None):
     controller_processor = processor.Processor(
         app_config=app_config, nr_processors=2, total_nr_qubits=total_nr_qubits, processor_index=0
     )
-    quantum_fourier_transform(controller_processor, total_nr_qubits)
+    _quantum_fourier_transform(controller_processor, total_nr_qubits)
 
 
-def quantum_fourier_transform(controller_processor, total_nr_qubits):
-    """
-    Perform a (distributed) quantum Fourier transformation.
-
-    Parameters
-    ----------
-    controller_processor: The controller processor in the cluster.
-    total_nr_qubits: The total number of qubits for the QFT.
-    """
-    add_qft_rotations(controller_processor, total_nr_qubits)
+def _quantum_fourier_transform(controller_processor, total_nr_qubits):
+    _add_qft_rotations(controller_processor, total_nr_qubits)
 
 
-def add_qft_rotations(controller_processor, remaining_nr_qubits):
-    """
-    Perform the controlled rotations part of the distributed quantum Fourier transformation.
-
-    Parameters
-    ----------
-    controller_processor: The controller processor in the cluster.
-    remaining_nr_qubits: The remaining number of qubits for which controlled rotations need
-        to be performed.
-    """
+def _add_qft_rotations(controller_processor, remaining_nr_qubits):
     if remaining_nr_qubits == 0:
         return
     remaining_nr_qubits -= 1
@@ -50,7 +34,7 @@ def add_qft_rotations(controller_processor, remaining_nr_qubits):
     #     controller_processor.controlled_phase(
     #         numpy.pi / 2 ** (remaining_nr_qubits - qubit), qubit, remaining_nr_qubits
     #     )
-    add_qft_rotations(controller_processor, remaining_nr_qubits)
+    _add_qft_rotations(controller_processor, remaining_nr_qubits)
 
 
 # TODO: Add swaps
